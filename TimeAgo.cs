c9cfb37ff95b11dde184aa6b51128e97efbd4705
Mahdi Hashemi <Mahdi008397@gmail.com>
2021-10-13 14:51:08 +0330

    first commit

---
+++ TimeAgo.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Time
+{
+    public static class TimeAgo
+    {
+        /// <summary>
+        /// /// The task of this method is to receive the present tense and the desired time to return a string as output. Like: "4 days ago"
+        /// </summary>
+        /// <param name="now">Solve input time</param>
+        /// <param name="compare">Input the desired time</param>
+        /// <returns></returns>
+        public static string GetTimeAgo(DateTime now, DateTime compare)
+        {
+            int year = now.Year - compare.Year;
+            int month = now.Month - compare.Month;
+            int days = now.Day - compare.Day;
+            int hours = now.Hour - compare.Hour;
+            int minutes = now.Minute - compare.Minute;
+            int seconds = now.Second - compare.Second;
+
+            if (year > 0)
+            {
+                return $"{year} سال پیش";
+            }
+            else if (month > 0)
+            {
+                return $"{month} ماه پیش";
+            }
+            else if (days > 0)
+            {
+                return $"{days} روز پیش";
+            }
+            else if (hours > 0)
+            {
+                return $"{hours} ساعت پیش";
+            }
+            else if (minutes > 0)
+            {
+                return $"{minutes} دقیقه پیش";
+            }
+            else if (seconds > 0)
+            {
+                return $"{seconds} ثانیه پیش";
+            }
+            else
+            {
+                return "لحضاتی پیش";
+            }
+        }
+    }
+}
